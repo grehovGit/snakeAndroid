6248af44c2191e0d19a958f26b15fc7483c7d439
grehovGit <grehov@ukr.net>
2016-11-21 18:47:54 +0200

    first commit

---
+++ src/com/mygdx/snakebox2d/android/AndroidLauncher.java
@@ -0,0 +1,22 @@
+package com.mygdx.snakebox2d.android;
+
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kingsnake.screens.KingSnake;
+//import com.mygdx.snakebox2d.SnakeBox2d;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KingSnake(), config);
+		
+	}
+}
+
+
+
